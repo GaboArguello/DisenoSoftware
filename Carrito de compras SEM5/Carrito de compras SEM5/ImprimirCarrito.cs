cf6a68f5cc4681bb42e0727c51d75e751b779996
GaboArguello <gabrielarguello12@outlook.com>
2015-06-15 18:32:08 -0600

    semana 5

---
+++ Carrito de compras SEM5/Carrito de compras SEM5/ImprimirCarrito.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carrito_de_compras_SEM5
+{
+    class ImprimirCarrito
+    {
+        public void ImprimeAlCarrito(List<Productos> productos)
+        {
+            Console.WriteLine(" Contenidos del carrito de compras:");
+            Console.WriteLine("***********************************");
+            foreach (Productos producto in productos)
+            {
+                Console.WriteLine("Identificador: {0}", producto.Id);
+                Console.WriteLine("Nombre: {0}", producto.nombre);
+                Console.WriteLine("Fecha: {0}", producto.fecha);
+                Console.WriteLine("***********************************"); 
+            }
+           
+        }
+
+        public void ImprimeProducto(Productos producto)
+        {
+            Console.WriteLine("Producto buscado: ");
+            Console.WriteLine("***********************************");
+            Console.WriteLine("Identificador: {0}", producto.Id);
+            Console.WriteLine("Nombre: {0}", producto.nombre);
+            Console.WriteLine("Fecha: {0}", producto.fecha);
+        }
+    }
+}
---
+++ Carrito de compras SEM5/Carrito de compras SEM5/Process.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carrito_de_compras_SEM5
+{
+    class Process
+    {
+        static void Main(string[] args)
+        {
+
+            LogicaDeAplicacion.Carrito Carrito = new LogicaDeAplicacion.Carrito();
+            ImprimirCarrito Imprime = new ImprimirCarrito();
+
+            Carrito.AgregarAlCarrito(new Productos(10199, "iPod Touch 64GB", "2015-06-06"));
+            Carrito.AgregarAlCarrito(new Productos(13100, "ChromeCast", "2015-03-06"));
+            Carrito.AgregarAlCarrito(new Productos(10299, "Laptop HB 256GB", "2015-07-06"));
+
+
+            Imprime.ImprimeAlCarrito(Carrito.ImprCarrito());
+
+            Imprime.ImprimeProducto(Carrito.BuscarBuscarPorIdentificacion(13100));
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
