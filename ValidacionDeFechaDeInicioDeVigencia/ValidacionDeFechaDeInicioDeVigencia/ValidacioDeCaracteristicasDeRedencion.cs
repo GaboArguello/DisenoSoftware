0388fac0787e61ec9aa252f04b4eedc699971328
GaboArguello <gabrielarguello12@outlook.com>
2015-07-13 20:48:58 -0600

    Semana 10 Estandar de programacion

---
+++ ValidacionDeFechaDeInicioDeVigencia/ValidacionDeFechaDeInicioDeVigencia/ValidacioDeCaracteristicasDeRedencion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidacionDeFechaDeInicioDeVigencia
+{
+    class ValidacioDeCaracteristicasDeRedencion
+    {
+
+        public Boolean LasCaracteristicasDeRedencionConValidas(Boolean permiteRedencion, 
+            double comision, int diasMinimosDeRetencion)
+        {
+            Boolean sonValidasLasCaracteristicasDeRedencion = false;
+
+            if (permiteRedencion.Equals(false) && comision.Equals(0) && diasMinimosDeRetencion.Equals(0))
+            {
+                sonValidasLasCaracteristicasDeRedencion = true;
+            }
+            else if (permiteRedencion.Equals(true) && comision > 0 && diasMinimosDeRetencion > 0)
+            {
+                sonValidasLasCaracteristicasDeRedencion = true;
+            }
+           
+                      
+            return sonValidasLasCaracteristicasDeRedencion;
+        }
+
+    }
+}
---
+++ ValidacionDeFechaDeInicioDeVigencia/ValidacionDeFechaDeInicioDeVigencia/ValidadorDeFechas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidacionDeFechaDeInicioDeVigencia
+{
+    public class ValidadorDeFechas
+    {
+        public Boolean LaFechaDeVigenciaEsValida(DateTime fechaDeVigencia, DateTime fechaDeRegistro)
+        {
+            Boolean laFechaEsValida = false;
+
+            if (fechaDeVigencia >= fechaDeRegistro)
+            {
+                laFechaEsValida = true;
+            }
+
+            return laFechaEsValida;
+        }
+    }
+}
